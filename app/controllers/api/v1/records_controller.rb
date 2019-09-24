6249e26f8ee69a88cfa9241303512c516c33e848
Val Cooper <valenciacooper89@gmail.com>
2019-09-24 10:45:10 -0400

    set up authentication

--- app/controllers/api/v1/records_controller.rb
+++ app/controllers/api/v1/records_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-      class RecordsController < ApplicationController
+    class RecordsController < ApplicationController
       before_action :set_record, only: [:show, :update, :destroy]
 
       # GET /records
